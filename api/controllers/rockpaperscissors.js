4e60ed62b34f471de4e6144b7e48f3310e19b425
Claire Peng <clairepeng94@gmail.com>
2024-03-06 07:40:51 +0000

    Adds refactored RPS Backend - TODO Test coverage for gameplay

--- api/controllers/rockpaperscissors.js
+++ api/controllers/rockpaperscissors.js
@@ -55,264 +55,61 @@ const concealedGameView = (populatedGame, viewerID) => {
 // ============ CONTROLLER ===============================
 const RockPaperScissorsController = {
   // ================== METHODS SHARED BY ALL GAMES ============================
-  // This Index method only shows the properties of allgames, and rounds
+  // Method to fetch all RockPaperScissors games
   Index: (req, res) => {
-    // using the select method to hide all gameboard details to prevent cheating.
-    RockPaperScissors.find()
-      .select({
-        title: 1,
-        endpoint: 1,
-        playerOne: 1,
-        playerTwo: 1,
-        winner: 1,
-        finished: 1,
-        round: 1,
-      })
-      .populate("playerOne", "_id username points")
-      .populate("playerTwo", "_id username points")
-      .populate("winner", "_id username points")
-      .exec((err, rockpaperscissorsGames) => {
-        if (err) {
-          throw err;
-        }
-        const token = TokenGenerator.jsonwebtoken(req.user_id);
-        res.setHeader("Cache-Control", "no-store, no-cache");
-        res.status(200).json({ games: rockpaperscissorsGames, token: token });
-      });
+    GamesController.Index(
+      req,
+      res,
+      RockPaperScissors,
+      concealedRockPaperScissorsView
+    );
   },
 
-  // This FindByID method conceals the game according to the viewer prior to returning the game data.
+  // Method to fetch a specific RockPaperScissors game by ID
   FindByID: (req, res) => {
-    const rockpaperscissorsID = req.params.id;
-    const userID = req.user_id; // userID of the viewer
-    const token = TokenGenerator.jsonwebtoken(req.user_id);
-
-    // ========= 1) Find the game ====================
-    RockPaperScissors.findById(rockpaperscissorsID)
-      .populate("playerOne", "_id username points")
-      .populate("playerTwo", "_id username points")
-      .populate("winner", "_id username points")
-      .exec((err, game) => {
-        if (err) {
-          throw err;
-        }
-
-        // Game not found
-        if (!game) {
-          return res
-            .status(404)
-            .json({ error: "Game not found", token: token });
-        }
-
-        // ======== 2) Conceal the boards according to who is looking (playerOne, playerTwo, outsider) ============
-        game = concealedGameView(game, userID);
-        res.setHeader("Cache-Control", "no-store, no-cache");
-        res.status(200).json({ game: game, token: token });
-      });
+    GamesController.FindByID(
+      req,
+      res,
+      RockPaperScissors,
+      concealedRockPaperScissorsView
+    );
   },
 
+  // Method to create a new RockPaperScissors game
   Create: async (req, res) => {
-    const userID = req.user_id;
-    const newRockPaperScissors = new RockPaperScissors({
-      playerOne: userID,
-    });
-
-    try {
-      const result = await newRockPaperScissors.save();
-      const populatedRockPaperScissors = await RockPaperScissors.populate(
-        result,
-        {
-          path: "playerOne",
-          select: "_id username points",
-        }
-      );
-      await RockPaperScissors.populate(populatedRockPaperScissors, {
-        path: "playerTwo",
-        select: "_id username points",
-      });
-      await RockPaperScissors.populate(populatedRockPaperScissors, {
-        path: "winner",
-        select: "_id username points",
-      });
-
-      const token = TokenGenerator.jsonwebtoken(req.user_id);
-      res.setHeader("Cache-Control", "no-store, no-cache");
-      res.status(201).json({ token: token, game: populatedRockPaperScissors });
-    } catch (error) {
-      console.log("Error in RPS.Create", error);
-      res.status(501).json(error);
-    }
+    GamesController.Create(req, res, RockPaperScissors);
+    console.log("Successfully created RPS Game");
   },
 
+  // Method to join a RockPaperScissors game
   Join: async (req, res) => {
-    const gameID = req.params.id;
-    const userID = req.user_id;
-    const token = TokenGenerator.jsonwebtoken(req.user_id);
-
-    try {
-      const game = await RockPaperScissors.findById(gameID);
-
-      // Game not found error
-      if (!game) {
-        return res.status(404).json({ error: "Game not found", token: token });
-      }
-      // Already joined game error
-      if (
-        userID == game.playerOne ||
-        (game.playerTwo && game.playerTwo == userID)
-      ) {
-        console.log("ERROR: ALREADY IN THIS GAME");
-        return res
-          .status(403)
-          .json({ error: "Already in this game", token: token });
-      }
-      // Game is full error
-      if (game.playerTwo) {
-        console.log("ERROR: GAME ALREADY FULL");
-        return res
-          .status(403)
-          .json({ error: "Game already full.", token: token });
-      } else {
-        // Join game
-        const joinedGame = await RockPaperScissors.findOneAndUpdate(
-          { _id: gameID },
-          {
-            $set: { playerTwo: userID },
-          },
-          { new: true }
-        )
-          .populate("playerOne", "_id username points")
-          .populate("playerTwo", "_id username points")
-          .populate("winner", "_id username points");
-
-        // ======== Conceal the boards according to who is looking (playerOne, playerTwo, outsider) ============
-        const concealedGame = concealedGameView(joinedGame, userID);
-        res.setHeader("Cache-Control", "no-store, no-cache");
-        res.status(200).json({ token: token, game: concealedGame });
-      }
-    } catch (error) {
-      console.log("Error in RPS.Join", error);
-      res.status(500).json(error);
-    }
+    GamesController.Join(
+      req,
+      res,
+      RockPaperScissors,
+      concealedRockPaperScissorsView
+    );
   },
 
+  // Method to forfeit a RockPaperScissors game
   Forfeit: async (req, res) => {
-    try {
-      const sessionUser = req.user_id;
-      const gameID = req.params.id;
-      const token = TokenGenerator.jsonwebtoken(req.user_id);
-      const game = await RockPaperScissors.findById(gameID);
-
-      // Game not found error
-      if (!game) {
-        return res.status(404).json({ error: "Game not found", token: token });
-      }
-      // Game aready finished error
-      if (game.finished) {
-        console.log("ERROR: GAME ALREADY FINISHED");
-        return res.status(403).json({
-          error: "Game already finished.",
-          token: token,
-        });
-      }
-      if (!game.playerTwo) {
-        console.log("ERROR: AWAITING PLAYER TWO");
-        return res.status(403).json({
-          error: "Awaiting player two. Please delete instead.",
-          token: token,
-        });
-      }
-
-      // Throw error if sessionUser is not in the game:
-      if (sessionUser != game.playerOne && sessionUser != game.playerTwo) {
-        // needs to be != due to Mongoose datatypes
-        console.log("ERROR: NON-PARTICIPANTS CANNOT FORFEIT");
-        return res.status(403).json({
-          error: "Only players can forfeit the game.",
-          token: token,
-        });
-      }
-      // needs to be ==  not === due to Mongoose datatypes
-      const winner =
-        sessionUser == game.playerOne ? game.playerTwo : game.playerOne;
-
-      const forfeitedGame = await RockPaperScissors.findOneAndUpdate(
-        { _id: gameID },
-        {
-          $push: { winner: winner },
-          $set: { finished: true },
-        },
-        { new: true }
-      )
-        .populate("playerOne", "_id username points")
-        .populate("playerTwo", "_id username points")
-        .populate("winner", "_id username points");
-
-      // ======== Conceal the boards according to who is looking (playerOne, playerTwo, outsider) ============
-      const concealedGame = concealedGameView(forfeitedGame, sessionUser);
-      res.setHeader("Cache-Control", "no-store, no-cache");
-      res.status(200).json({ token: token, game: concealedGame });
-    } catch (error) {
-      console.error("Error forfeiting: ", error);
-      res.status(500).json(error);
-    }
+    GamesController.Forfeit(
+      req,
+      res,
+      RockPaperScissors,
+      concealedRockPaperScissorsView
+    );
   },
 
+  // Method to delete a RockPaperScissors game
   Delete: async (req, res) => {
-    try {
-      const sessionUser = req.user_id;
-      const gameID = req.params.id;
-      const token = TokenGenerator.jsonwebtoken(req.user_id);
-      const game = await RockPaperScissors.findById(gameID);
-
-      // Game not found error
-      if (!game) {
-        return res.status(404).json({ error: "Game not found", token: token });
-      }
-      // Throw error if sessionUser is not playerOne (host)
-      if (sessionUser != game.playerOne) {
-        // needs to be != due to Mongoose datatypes
-        console.log("ERROR: ONLY HOSTS CAN DELETE GAMES");
-        return res.status(403).json({
-          error: "Only hosts can delete the game.",
-          token: token,
-        });
-      }
-      // Throw error if game is full (has playerTwo):
-      if (game.playerTwo) {
-        console.log("ERROR: CANNOT DELETE NON-OPEN GAMES");
-        return res.status(403).json({
-          error: "Only games awaiting player Two can be deleted.",
-          token: token,
-        });
-      }
-
-      // Delete the game
-      await RockPaperScissors.findByIdAndDelete(gameID);
-
-      // Get the updated game list
-      const updatedGames = await RockPaperScissors.find()
-        .select({
-          title: 1,
-          endpoint: 1,
-          playerOne: 1,
-          playerTwo: 1,
-          winner: 1,
-          finished: 1,
-          round: 1,
-        })
-        .populate("playerOne", "_id username points")
-        .populate("playerTwo", "_id username points")
-        .populate("winner", "_id username points");
-
-      res.setHeader("Cache-Control", "no-store, no-cache");
-      res.status(200).json({ token: token, games: updatedGames });
-    } catch (error) {
-      console.error("Error deleting: ", error);
-      res.status(500).json(error);
-    }
+    GamesController.Delete(
+      req,
+      res,
+      RockPaperScissors,
+      concealedRockPaperScissorsView
+    );
   },
-
   // ===================== ROCK PAPER SCISSORS SPECIFIC GAMEPLAY METHODS ============================
   SubmitChoice: async (req, res) => {
     const gameID = req.params.id;
