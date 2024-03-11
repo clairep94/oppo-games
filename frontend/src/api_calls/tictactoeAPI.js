f7a9710087304811f154ec719d029a1034cfd80c
Claire Peng <clairepeng94@gmail.com>
2024-03-11 12:10:56 +0000

    Fixes bug on 'your games' view

--- frontend/src/api_calls/tictactoeAPI.js
+++ frontend/src/api_calls/tictactoeAPI.js
@@ -1,110 +1,105 @@
-const endpoint = '/tictactoe';
+const endpoint = "/tictactoe";
 
 const newGame = async (token) => {
-    try {
-        const response = await fetch(`${endpoint}`, {
-            method: 'post',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`,
-            },
-        });
-        const newGameData = await response.json();
-        return newGameData;
-    } catch (error) {
-        console.error("TictactoeAPI.newGame:", error);
-        throw error;
-    }
-}
+  try {
+    const response = await fetch(`${endpoint}`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const newGameData = await response.json();
+    return newGameData;
+  } catch (error) {
+    console.error("TictactoeAPI.newGame:", error);
+    throw error;
+  }
+};
 
 const fetchGame = async (token, id) => {
-    try {
+  try {
     const response = await fetch(`${endpoint}/${id}`, {
-        headers: {
+      headers: {
         Authorization: `Bearer ${token}`,
-        },
-        });
-        const gameData = await response.json();
-        return gameData;
-        
-    } catch (error) {
-        console.error("TictactoeAPI.findUser:", error);
-        throw error;
-    }
-
-}
+      },
+    });
+    const gameData = await response.json();
+    return gameData;
+  } catch (error) {
+    console.error("TictactoeAPI.findUser:", error);
+    throw error;
+  }
+};
 
 const allGames = async (token) => {
-    try {
-        const response = await fetch(`${endpoint}`, {
-            headers: {
-            Authorization: `Bearer ${token}`,
-        },
-        });
-        const gameData = await response.json();
-        console.log(gameData)
-        return gameData;
-    } catch (error) {
-        console.error("TictactoeAPI.allUsers:", error);
-        throw error;
-    }
-}
+  try {
+    const response = await fetch(`${endpoint}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const gameData = await response.json();
+    console.log(gameData);
+    return gameData;
+  } catch (error) {
+    console.error("TictactoeAPI.allUsers:", error);
+    throw error;
+  }
+};
 
 const placePiece = async (token, id, movePayload) => {
-    // Payload = {row: row, col: col}
-    try {
-        const response = await fetch(`${endpoint}/${id}/place_piece`, {
-            method: 'put',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`,
-
-            },
-            body: JSON.stringify(movePayload)
-        });
-        const gameData = await response.json();
-        return gameData;
-    } catch (error) {
-        console.error("TictactoeAPI.placePiece:", error);
-        throw error;
-    }
-}
+  // Payload = {row: row, col: col}
+  try {
+    const response = await fetch(`${endpoint}/${id}/place_piece`, {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(movePayload),
+    });
+    const gameData = await response.json();
+    return gameData;
+  } catch (error) {
+    console.error("TictactoeAPI.placePiece:", error);
+    throw error;
+  }
+};
 
 const joinGame = async (token, id) => {
-    // Payload = {row: row, col: col}
-    try {
-        const response = await fetch(`${endpoint}/${id}/join`, {
-            method: 'put',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`,
-
-            },
-        });
-        const gameData = await response.json();
-        return gameData;
-    } catch (error) {
-        console.error("TictactoeAPI.join:", error);
-        throw error;
-    }
-}
-
+  // Payload = {row: row, col: col}
+  try {
+    const response = await fetch(`${endpoint}/${id}/join`, {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const gameData = await response.json();
+    return gameData;
+  } catch (error) {
+    console.error("TictactoeAPI.join:", error);
+    throw error;
+  }
+};
 
 const forfeitGame = async (token, id) => {
-    try {
-        const response = await fetch(`${endpoint}/${id}/forfeit`, {
-            method: 'put',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`,
-            },
-        });
-        const gameData = await response.json();
-        return gameData;
-    } catch (error) {
-        console.error("TictactoeAPI.forfeitGame:", error);
-        throw error;
-    }
-}
+  try {
+    const response = await fetch(`${endpoint}/${id}/forfeit`, {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const gameData = await response.json();
+    return gameData;
+  } catch (error) {
+    console.error("TictactoeAPI.forfeitGame:", error);
+    throw error;
+  }
+};
 
-export { newGame, fetchGame, allGames, placePiece, joinGame, forfeitGame }
+export { newGame, fetchGame, allGames, placePiece, joinGame, forfeitGame };
