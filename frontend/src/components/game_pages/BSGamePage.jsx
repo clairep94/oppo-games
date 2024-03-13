fb02139e483691cc2cf46cdba860243e7d8a55ca
Claire Peng <clairepeng94@gmail.com>
2024-03-13 20:37:55 +0000

    Adds WIP BSGamePage -- attempting draggable but will change to button after this

--- frontend/src/components/game_pages/BSGamePage.jsx
+++ frontend/src/components/game_pages/BSGamePage.jsx
@@ -19,6 +19,11 @@ export default function BSGamePage({ token, setToken, sessionUserID, sessionUser
         console.log(`Clicked on row ${row} and column ${col}`);
       };
 
+    const handleSubmit = () => {
+        // convert board buttons into an array
+        // if ship is snapped to board,
+    }
+
 
     // ============================= STATE VARIABLES ===========================================
     // --------- Session & Game ID ----------
@@ -92,202 +97,6 @@ export default function BSGamePage({ token, setToken, sessionUserID, sessionUser
             fetchGameData()
         }
     }, [])
-
-    // ======================= LOADING MESSAGES =============================================
-    const [messages, setMessages] = useState([]);
-    const [newMessage, setNewMessage] = useState("");
-    const [receivedMessage, setReceivedMessage] = useState("");
-
-
-    useState(() => {
-        fetchMessages(gameID, token)
-        .then(messagesData => {
-            setMessages(messagesData.allMessages);
-        })
-    }, [])
-
-    // Scroll to the last message when messages change
-    const messagesEndRef = useRef(null);
-
-    useEffect(() => {
-        messagesEndRef.current?.scrollIntoView({ behavior: "smooth", block: "end" });
-    }, [messages]);
-
-
-    // ============ SESSION USER GAMEPLAY =================================
-    // Function to place a piece on the gameboard
-    const handlePlacePiece = async(row, col) => {
-        const coordinates = `${row}${col}`
-        console.log(`Coordinates: ${row} ${col}`)
-
-        // check if there is a second player:
-        if (!game.playerTwo) {
-            console.log("Must wait for player two!")
-            setErrorMessage("You must wait for player two to join")
-
-        // check if the space is already occupied:
-        } else if (game.xPlacements.includes(coordinates) || game.oPlacements.includes(coordinates)){
-            console.log("already a piece here")
-            setErrorMessage("There is already a piece here!")
-        
-        // check if game is already over:
-        } else if (game.finished) {
-            setErrorMessage("The game is already over!")
-
-        // check if the sessionUserID === whoseTurn._id -> if not, setErrorMessage
-        } else if (sessionUserID !== whoseTurn._id) {
-            if (sessionUserID === game.playerOne._id || sessionUserID === game.playerTwo._id){
-                setErrorMessage("It's not your turn!")
-            } else {
-                setErrorMessage("You're not in this game!")
-            }
-        
-        // if all checks pass, place the piece and update game with returned game data
-        } else {
-            if (token) {
-                const movePayload = {row: row, col: col}
-                placePiece(token, gameID, movePayload)
-                .then(gameData => {
-                    window.localStorage.setItem("token", gameData.token);
-                    setToken(window.localStorage.getItem("token"));
-
-                    const updatedGame = gameData.game;
-    
-                    setGame(gameData.game);
-                    setWhoseTurn((gameData.game.turn % 2 === 0) ? gameData.game.playerOne : gameData.game.playerTwo)
-                    findWinMessage(gameData.game)
-                    setErrorMessage("")
-
-                    socket.current.emit("place-piece", {gameID, updatedGame})
-                })
-            }
-        }
-    }
-
-    // ============ FORFEIT GAME ===============================
-    // Function to forfeit game -- first sets warning message, then forfeits game.
-    const handleForfeit = async (event) => {
-        event.preventDefault();
-
-        if (forfeitButtonMessage === "Forfeit Game"){
-            setForfeitButtonMessage("Are you sure?")
-        }
-        if (token && (forfeitButtonMessage === "Are you sure?")) {
-            forfeitGame(token, gameID)
-            .then(gameData => {
-                window.localStorage.setItem("token", gameData.token);
-                setToken(window.localStorage.getItem("token"));
-
-                const updatedGame = gameData.game
-                setGame(gameData.game)
-                findWinMessage(gameData.game)
-
-                socket.current.emit("forfeit-game", {gameID, updatedGame})
-
-                ;})
-            }}
-
-
-    // =================== OPPONENT GAMEPLAY ========================================
-    // ------------ Socket setup: -------------------------------
-    const socket = useRef() //menu, chatwindow
-    const [onlineUsers, setOnlineUsers] = useState(null);
-
-    // --------- CONNECTING TO SOCKET & ONLINE USERS -----------------
-    // Connect to socket.io when users visit the messages page //TODO lift this to app after login?
-    useEffect(()=> {
-        socket.current = io('http://localhost:8800'); // this is the socket port
-        socket.current.emit("add-new-user", sessionUserID, gameID); // send the sessionUserID to the socket server
-        socket.current.emit("create-game-room", gameID);
-        socket.current.on('get-users', (users)=>{
-            setOnlineUsers(users)}) // get the onlineUsers, which should now include the sessionUserID
-        
-        //---------------- Receiving new move -------------------
-        socket.current.on("receive-game-update", ({ gameID, gameState }) => {
-            console.log("received game from socket", gameState);
-            setGame(gameState);
-            setWhoseTurn(
-                gameState.turn % 2 === 0
-                ? gameState.playerOne
-                : gameState.playerTwo
-            );
-            findWinMessage(gameState);
-            setErrorMessage("");
-        });
-
-        //---------------- Receiving forfeit -------------------
-        socket.current.on("receive-forfeit-game", ({ gameID, gameState }) => {
-            console.log("received forfeit game from socket", gameState);
-            setGame(gameState);
-            setWhoseTurn(
-                gameState.turn % 2 === 0
-                ? gameState.playerOne
-                : gameState.playerTwo
-            );
-            setWinMessage(gameState); // setting a "opponent forfeited, you win" message makes both windows have this message.
-            setErrorMessage("");
-        });
-
-
-        //---------------- Receiving messages -------------------
-        socket.current.on("receive-message", ({gameID, receivedMessage}) => {
-            console.log("received message from socket", receivedMessage);
-            // const newMessage = {
-            //     _id: receivedMessage._id,
-            //     gameID: receivedMessage.gameID,
-            //     author: receivedMessage.author,
-            //     body: receivedMessage.body
-            // }
-            setMessages([...messages, receivedMessage])
-
-            // setMessages((prevMessages) => prevMessages.concat(newMessage));
-            // setMessages(currentMessages)
-
-        })
-
-    }, [sessionUserID])
-
-    
-
-
-    // ---------------------- SENDING A MESSAGE -----------------------------
-    // Change newMessage when something is written to InputEmoji 
-    const handleChange=(newMessage)=>{
-        setNewMessage(newMessage); // --> Don't need to use event.target because it is not a 'form'
-    }
-
-    // Sending a message to both the Backend API and socket.io 
-    const handleSend = async (event) => {
-
-        const messageToSend = {
-            gameID: gameID,
-            author: sessionUserID,
-            body: newMessage
-        }
-
-        console.log(messageToSend)
-
-    // Send the message to the database:
-    if (newMessage.trim()) { // check that there is a conversationPartner and that newMessage is not all whitespaces
-        addMessage(messageToSend, token)
-            .then(sentMessageData => {
-                const sentMessage = sentMessageData.newMessage
-                // add newMessage to the messages array:
-                setMessages([...messages, sentMessage]);
-                // send message to the socket server:
-                socket.current.emit("send-message", {gameID, sentMessage})
-                // clear newMessage: -- check if this needs to be after
-                setNewMessage("");
-      })
-    }
-  }
-
-
-    // ----------- SCROLL TO THE LAST MESSAGE --------------
-    // const div = useRef(null)
-    // useEffect(()=> {
-    //     div.current?.scrollIntoView({ behavior: "smooth", block:"end" });
-    // },[messages])
 
 
     // ============================== TAILWIND ==============================================
@@ -393,17 +202,26 @@ export default function BSGamePage({ token, setToken, sessionUserID, sessionUser
                         </div>
                         
 
-                        
-                        <button onClick={() => {setShipDirectionHorizontal(!shipDirectionHorizontal)}} className="bg-black/70 p-4 w-[13rem] rounded-lg">
-                            Toggle Ship Direction
-                        </button>
+                        <div className="flex flex-row space-x-2">
+                            
+                            {/* TOGGLE SHIPS BUTTON */}
+                            <button onClick={() => {setShipDirectionHorizontal(!shipDirectionHorizontal)}} className="bg-black/70 p-4 w-[13rem] rounded-lg">
+                                Toggle Ship Direction
+                            </button>
 
-                        {/* FORFEIT BUTTON-- only shows if sessionUser is a player && game is not over */}
-                        {/* {!game.finished && game.playerTwo && (sessionUserID === game.playerOne._id || sessionUserID === game.playerTwo._id) &&
-                            (<button onClick={handleForfeit} className="bg-black/70 p-4 w-[13rem] rounded-lg">
-                                {forfeitButtonMessage}
-                            </button>)
-                        } */}
+                            {/* SUBMIT SHIP PLACEMENTS */}
+                            <button onClick={() => {console.log("Submit Ships")}} className="bg-black/70 p-4 w-[13rem] rounded-lg">
+                                Submit ships
+                            </button>
+                            {/* FORFEIT BUTTON-- only shows if sessionUser is a player && game is not over */}
+                            {!game.finished && game.playerTwo && (sessionUserID === game.playerOne._id || sessionUserID === game.playerTwo._id) &&
+                                (<button onClick={()=>{console.log("Forfeit")}} className="bg-black/70 p-4 w-[13rem] rounded-lg">
+                                    {forfeitButtonMessage}
+                                </button>)
+                            }
+
+                        </div>
+
                         
                         {errorMessage && 
                             <h2 className="text-red-600/80 font-semibold text-2xl p-3">{errorMessage}</h2>}
@@ -425,25 +243,19 @@ export default function BSGamePage({ token, setToken, sessionUserID, sessionUser
                     <div className="flex flex-col bg-gray-600/40 rounded-[1rem] h-full overflow-y-auto px-4 py-2 border-2 space-y-1 border-white/20">
                         {/* MESSAGES */}
                         <div className="flex flex-col h-full overflow-auto px-1 ">
-                            {messages?.length === 0 ? (
+                            
                                     <p className="text-white/80">Write a message...</p>
-                                ) : (
-                                    messages?.map(message => (
-                                        <p key={message._id} className={'' + (message.author._id === sessionUserID ? 'text-yellow-400/80' : '')}>
-                                            <span className="font-bold">{message.author.username}:</span> {' '}{message.body}
-                                        </p>
-                                    ))
-                            )}
+                           
                             {/* Empty div to scroll to the last message */}
-                            <div ref={messagesEndRef}></div>
+                    
                         </div>
                         <div className="flex flex-col h-2/5">
                         {/* Input Field from React Lib for writing a new message, can add emojis */}
                             <InputEmoji 
-                                value={newMessage? newMessage : ""}
-                                cleanOnEnter
-                                onChange={handleChange}
-                                onEnter={handleSend}
+                                // value={newMessage? newMessage : ""}
+                                // cleanOnEnter
+                                // onChange={handleChange}
+                                // onEnter={handleSend}
                                 placeholder='Type a message...'
                             />
                         </div>
@@ -461,25 +273,3 @@ export default function BSGamePage({ token, setToken, sessionUserID, sessionUser
 
 
 // =========== SUPPORTIVE COMPONENTS: ==================================== //
-
-const TicTacToeBoard = ({ gameBoard, onButtonClick }) => {
-    const rows = ["A", "B", "C"];
-
-    return (
-        <div className="tictactoe-board">
-            {rows.map(row => (
-                <div key={row} className="tictactoe-row flex">
-                    {Object.keys(gameBoard[row]).map(col => (
-                        <button
-                            key={col}
-                            className="h-[6rem] w-[6rem] mb-2 text-[2rem] bg-slate-400/40 border-2 border-white/20 shadow-sm text-black/80 rounded-md mr-1 hover:bg-slate-400 font-semibold flex items-center justify-center"
-                            onClick={() => onButtonClick(row, col)}
-                        >
-                            {gameBoard[row][col]}
-                        </button>
-                    ))}
-                </div>
-            ))}
-        </div>
-    );
-};
