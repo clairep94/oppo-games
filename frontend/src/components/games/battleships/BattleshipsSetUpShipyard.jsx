1fb29076db6b68710218057c02f45e123fa7b9d7
Claire Peng <clairepeng94@gmail.com>
2024-03-16 20:33:42 +0000

    Adds bug fix for shipyard divs

--- frontend/src/components/games/battleships/BattleshipsSetUpShipyard.jsx
+++ frontend/src/components/games/battleships/BattleshipsSetUpShipyard.jsx
@@ -1,23 +1,21 @@
 import React from 'react'
 
 export default function BattleshipsSetUpShipyard({placementShipyard, shipDirectionHorizontal, selectShip}) {
+  const unitSize = 8
   return (
     <div className={`flex flex-${shipDirectionHorizontal ? "col" : "row"}`}>
       {placementShipyard.map((ship) => (
-        
-        // SHIP DIV
-        <div 
-        className={`flex flex-${shipDirectionHorizontal ? "row" : "col"} m${shipDirectionHorizontal ? "y" : "x"}-1`}
-        key={ship.title}
-        onClick={() => selectShip(ship)}
-        >
-          {/* SHIP DIV UNITS  */}
-          {Array.from({ length: ship.units }, (_, index) => (
-            <div
-                key={index}
-                className={`w-8 h-8 bg-red-400/70 mr-[0px]`}/>
-          ))}
-        </div>
+        // SHIP DIV        
+        <div
+          key={ship.title}
+          className={`
+          ${shipDirectionHorizontal ? "w" : "h"}-${unitSize * ship.units}
+          ${shipDirectionHorizontal ? "h" : "w"}-${unitSize}
+          bg-red-400/70
+          mr-2 mb-2
+          `}
+          onClick={() => selectShip(ship)}
+        />
 
       ))}
     </div>
